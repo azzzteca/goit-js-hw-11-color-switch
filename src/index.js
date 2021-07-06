a5d1232f7047dae1da45aadd51f5cc46a8a2a3f2
Andrii Shovkun <andrii.shovkun@gmail.com>
2021-07-06 11:54:28 +0300

    task done

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// import './sass/main.scss';
+const colors = ['#FFFFFF', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548'];
+
+const refs = {
+  btnStart: document.querySelector('button[data-action="start"]'),
+  btnStop: document.querySelector('button[data-action="stop"]'),
+  body: document.querySelector('body'),
+};
+
+let timer = 0;
+
+refs.btnStart.addEventListener('click', startChangeColor);
+
+function startChangeColor(evt) {
+  refs.btnStop.addEventListener('click', stopChangeColor);
+
+  timer = setInterval(() => {
+    refs.btnStart.removeEventListener('click', startChangeColor);
+    refs.body.style.backgroundColor = colors[Math.floor(Math.random() * (colors.length + 1))];
+  }, 1000);
+}
+
+function stopChangeColor() {
+  refs.btnStop.removeEventListener('click', stopChangeColor);
+  clearInterval(timer);
+  refs.btnStart.addEventListener('click', startChangeColor);
+}
